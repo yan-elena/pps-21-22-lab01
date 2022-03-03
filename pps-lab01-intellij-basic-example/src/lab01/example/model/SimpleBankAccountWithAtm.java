dcac84aab99dc8c04bd15c02a7299481b9c39679
Elena Yan <elena.yan@studio.unibo.it>
2022-03-03 20:04:14 +0100

    add documentation

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -1,5 +1,9 @@
 package lab01.example.model;
 
+/**
+ * This class represent a particular instance of a BankAccount.
+ * In particular, it's an ATM Simple Bank Account, while for each withdrawal it requires a fee
+ */
 public class SimpleBankAccountWithAtm extends AbstractBankAccount {
 
     public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
